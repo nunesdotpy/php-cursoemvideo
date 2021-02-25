8ce13ec2b79742e53772c3b85ee07251f1508fcb
nunes.py <nunesdotpy@users.noreply.github.com>
2021-02-25 08:59:54 -0300

    aula04

---
+++ aula05/operacoes aritmeticas.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../_css/estilo.css">
+    <title>Operações Aritméticas</title>
+</head>
+<body>
+<div>
+    <?php
+        $n1 = 3;
+        $n2 = 2;
+        $s = $n1 + $n2;
+        echo "A soma entre $n1 e $n2 é igual a $s";
+    
+    
+    ?>
+
+</div>    
+</body>
+</html>
---
+++ aula4/variaveis.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../_css/estilo.css">
+    <title>Variáveis</title>
+</head>
+<body>
+<div>
+    <?php
+    $nome = "Gustavo";
+    $idade = 36;
+    echo "$nome tem $idade anos";
+    ?>
+</div>
+</body>
+</html>
