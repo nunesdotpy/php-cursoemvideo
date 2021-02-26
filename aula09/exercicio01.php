09e8e99837f92e5fd938926c080a251c09a15c14
nunes.py <nunesdotpy@users.noreply.github.com>
2021-02-26 01:44:12 -0300

    aula09

---
+++ aula09/exercicio01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../_css/estilo.css">
+    <title></title>
+</head>
+<body>
+<div>
+    <form method="get" action="resultado01.php">
+        Ano de Nascimento: <input type="number" name="ano">
+        <input type="submit" value="Enviar">
+    </form>
+</div>
+</body>
+</html>
