f8dd4973673d356aed066907639fb2e200dc374b
nunes.py <nunesdotpy@users.noreply.github.com>
2021-02-26 02:21:30 -0300

    aula10 exercicio01
    problema resolviso e switch funcionando com html e php com codigos mais enxutos

--- aula10/resultado01.php
+++ aula10/resultado01.php
@@ -10,27 +10,20 @@
     <?php
         $num = $_GET["nu1"];
         $o = $_GET["op"];
-
         switch ($o){
             case 1:
-                if ($o=="dobro"){
                     $num *= 2;
                     echo "O dobro de ". $_GET["nu1"] ." é $num";
-                }
                 break;
 
             case 2:
-                if ($o=="cubo"){
                     $num *= $num;
                     echo "O cubo de ". $_GET["nu1"] ." é $num";
-                }
                 break;
             
             case 3:
-                if ($o=="raiz"){
                     $num = sqrt($num);
                     echo "A raiz de ". $_GET["nu1"] ." é $num";
-                }
             break;
 
             default:
