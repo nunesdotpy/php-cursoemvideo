bc766369a129fc503c23f7a1fd1babee3944af22
Vitor Nunes <nunesdotpy@users.noreply.github.com>
2021-03-08 17:00:32 -0300

    aula sobre Do While

---
+++ aula12/exercicio01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../_css/estilo.css">
+    <title></title>
+</head>
+<body>
+<div>
+    <?php
+    $c = 1;
+
+    do{
+        echo "$c ";
+        $c++;
+    }while($c<=10);
+    ?>
+</div>
+</body>
+</html>
