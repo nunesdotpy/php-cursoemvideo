412b9f23b40cd287e87c0adcfb1b4bb3dff1502b
Vitor Nunes <nunesdotpy@users.noreply.github.com>
2021-03-10 12:09:49 -0300

    funçoes pt2

---
+++ aula15/funcao.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../_css/estilo.css">
+    <title></title>
+</head>
+<body>
+<div>
+    <?php
+        include "funcoes.php";
+        echo "<h1>Testando novas funções</h1>";
+        ola();
+        mostraValor(4);
+        echo "<h2>Finalizando Programa...</h2>"
+    ?>
+</div>
+</body>
+</html>
---
+++ aula15/valor.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../_css/estilo.css">
+    <title></title>
+</head>
+<body>
+<div>
+    <?php
+        function teste ($x){
+            $x += 2;
+            echo $x;
+        }
+
+        $a = 3;
+        teste($a);
+        echo "<br>$a";
+    ?>
+</div>
+</body>
+</html>
